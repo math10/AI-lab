fe31951081ddf6de5f523e314a4d4ffbe121fe63
math10 <sanim_10@yahoo.com>
2015-03-16 22:41:22 +0600

    Revert "checked"
    This reverts commit 79e265fbf293ed02cfdf4726177da4ebdf849404.

--- addElementlast.pl
+++
@@ -1,5 +0,0 @@
-/*
-Write a procedure to add an element at the end of a list.
-*/
-addelLast(L1,L2):- write('Element '),read(X),write('\n'),
-		append(L1,[X],L2).
--- delLastNel.pl
+++
@@ -1,8 +0,0 @@
-/*
-Write a procedure to delete the last n elements from a list.
-*/
-
-delLastNel(L1,N,L):-length(L1,N1),N2 is N1-N,write(N2),call1(L1,N2,L).
-
-call1([H|_],1,[H]).
-call1([H|T],N,L):-N>1,N1 is N-1,call1(T,N1,L1),L = [H|L1].
